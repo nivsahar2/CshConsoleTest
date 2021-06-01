2e0671cb5f24bbe4485f542e57965bf938b09b22
Niv Sahar <71365529+nivsahar2@users.noreply.github.com>
2021-06-01 21:33:21 +0300

    Update CommandsTest.cs

--- CommandsTest/CommandsTest.cs
+++ CommandsTest/CommandsTest.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 
 using CshConsoleAPI;
@@ -12,11 +12,36 @@ namespace CshConsoleTest
 	{
 		public const string CMD_ECHO_PARAM= "hello echo";
 		public const string CMD_LINE_ECHO= "echo(hello world)";
+		public const string CMD_LINE_ADD = "add(3,5)";
+		public const string CMD_LINE_SUB = "sub(3,5,2)";
+		public const string CMD_LINE_MUL = "mul(3,5,2)";
+		public const string CMD_LINE_DIV = "div(3,5,2)";
 
 		private static bool CommandEcho(string[] parameters)
 		{
 			return (true);
 		}
+
+		private static bool CommandAdd(string[] parameters)
+		{
+			return (true);
+		}
+
+		private static bool CommandSub(string[] parameters)
+		{
+			return (true);
+		}
+
+		private static bool CommandMul(string[] parameters)
+		{
+			return (true);
+		}
+
+		private static bool CommandDiv(string[] parameters)
+		{
+			return (true);
+		}
+
 
 		[TestMethod]
 		public void TestMethod1()
@@ -46,7 +71,7 @@ namespace CshConsoleTest
 
 		//	Test the commands list using the echo command function.
 		[TestMethod]
-		public void TestCommandEcho()
+		public void TestCommandAll()
 		{
 			//	Check creating a new commands list.
 			Commands pCommands = CommandsApi.CommandsInit();
@@ -57,7 +82,64 @@ namespace CshConsoleTest
 
 			//	Execute the echo command in the command list.
 			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_ECHO));
+
+			//	Check adding echo command to commands list.
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_ADD, CommandAdd));
+
+			//	Execute the echo command in the command list.
+			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_ADD));
+
+			//	Check adding echo command to commands list.
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_SUB, CommandSub));
+
+			//	Execute the echo command in the command list.
+			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_SUB));
+
+			//	Check adding echo command to commands list.
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_MUL, CommandMul));
+
+			//	Execute the echo command in the command list.
+			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_MUL));
+
+			//	Check adding echo command to commands list.
+			Assert.IsTrue(CommandsApi.CommandAdd(ref pCommands, AppCommands.CMD_DIV, CommandDiv));
+
+			//	Execute the echo command in the command list.
+			Assert.IsTrue(CommandsApi.CommandExec(ref pCommands, CMD_LINE_DIV));
 			return;
 		}
+
+
+		[TestMethod]
+		public void TestAdd()
+		{
+			string[] parameters = CommandsApi.CommandParams(CMD_LINE_ADD);
+			Assert.IsTrue(AppCommands.CommandAdd(parameters));
+			return;
+		}
+
+		[TestMethod]
+		public void TestSub()
+		{
+			string[] parameters = CommandsApi.CommandParams(CMD_LINE_SUB);
+			Assert.IsTrue(AppCommands.CommandSub(parameters));
+			return;
+		}
+		[TestMethod]
+		public void TestMul()
+		{
+			string[] parameters = CommandsApi.CommandParams(CMD_LINE_MUL);
+			Assert.IsTrue(AppCommands.CommandMul(parameters));
+			return;
+		}
+		[TestMethod]
+		public void TestDiv()
+		{
+			string[] parameters = CommandsApi.CommandParams(CMD_LINE_DIV);
+			Assert.IsTrue(AppCommands.CommandDiv(parameters));
+			return;
+		}
+
 	};
 }
+
